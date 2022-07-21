983aa8db9b40344f8b3de0934281c1909e729397
hmc <670937448@qq.com>
2022-07-21 12:12:09 +0800

    update

--- plugin/gmsr/info.go
+++ plugin/gmsr/info.go
@@ -17,34 +17,24 @@ var engine = control.Register("gmsr", &ctrl.Options[*zero.Ctx]{
 
 // 查成分的
 func init() {
-	// engine.OnRegex(`^>vup info\s?(.{1,25})$`).SetBlock(true).
-	// 	Handle(func(ctx *zero.Ctx) {
-	// 		keyword := ctx.State["regex_matched"].([]string)[1]
-	// 		res, err := search(keyword)
-	// 		if err != nil {
-	// 			ctx.SendChain(message.Text("ERROR:", err))
-	// 			return
-	// 		}
-	// 		id := strconv.FormatInt(res[0].Mid, 10)
-	// 		// 获取详情
-	// 		fo, err := fansapi(id)
-	// 		if err != nil {
-	// 			ctx.SendChain(message.Text("ERROR:", err))
-	// 			return
-	// 		}
-	// 		ctx.SendChain(message.Text(
-	// 			"search: ", fo.Mid, "\n",
-	// 			"名字: ", fo.Uname, "\n",
-	// 			"当前粉丝数: ", fo.Follower, "\n",
-	// 			"24h涨粉数: ", fo.Rise, "\n",
-	// 			"视频投稿数: ", fo.Video, "\n",
-	// 			"直播间id: ", fo.Roomid, "\n",
-	// 			"舰队: ", fo.GuardNum, "\n",
-	// 			"直播总排名: ", fo.AreaRank, "\n",
-	// 			"数据来源: ", "https://vtbs.moe/detail/", fo.Mid, "\n",
-	// 			"数据获取时间: ", time.Now().Format("2006-01-02 15:04:05"),
-	// 		))
-	// 	})
+	engine.OnRegex(`^查询\s?(.{1,15})$`).SetBlock(true).
+		Handle(func(ctx *zero.Ctx) {
+			keyword := ctx.State["regex_matched"].([]string)[1]
+
+			// ctx.SendChain(message.Text(
+			// 	"search: ", fo.Mid, "\n",
+			// 	"名字: ", fo.Uname, "\n",
+			// 	"当前粉丝数: ", fo.Follower, "\n",
+			// 	"24h涨粉数: ", fo.Rise, "\n",
+			// 	"视频投稿数: ", fo.Video, "\n",
+			// 	"直播间id: ", fo.Roomid, "\n",
+			// 	"舰队: ", fo.GuardNum, "\n",
+			// 	"直播总排名: ", fo.AreaRank, "\n",
+			// 	"数据来源: ", "https://vtbs.moe/detail/", fo.Mid, "\n",
+			// 	"数据获取时间: ", time.Now().Format("2006-01-02 15:04:05"),
+			// ))
+			ctx.SendChain(message.Text(keyword))
+		})
 
 	engine.OnFullMatch("zbphelloworld").SetBlock(true).
 		Handle(func(ctx *zero.Ctx) {
